44f185b5a5d3c78334a35acffea457f08114d433
Ajit Tupe <ajittupe2548@gmail.com>
2021-11-28 20:52:54 +0530

    Added : Event handling

--- src/Components/CHome.js
+++ src/Components/CHome.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 
 class CHome extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+        this.handleClick = this.handleClick.bind(this);    // Third : Binding in constructor(Recommended)
+    }
+
+    handleClick() {
+    // handleClick = () => {    Fourth : Converting simple function to arrow function
+    this.setState(prevState => ({
+        isToggleOn: !prevState.isToggleOn
+    }));
+    }
+
     render() {
-        return (
-            <div>
-                <h1 className="heading">Hi from home page class</h1>
-            </div>
-        )
+    return (
+        // <button onClick={this.handleClick.bind(this)}>   First : Binding in element itself
+        // <button onClick={() => this.handleClick()}>  Second : Using arrow function
+        <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+    );
     }
 }
 
