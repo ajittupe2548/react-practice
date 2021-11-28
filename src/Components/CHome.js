f3f361b06dc25b6f72f128e304bd71a2d5ba7f9b
Ajit Tupe <ajittupe2548@gmail.com>
2021-11-28 21:00:49 +0530

    Added : First approach

--- src/Components/CHome.js
+++ src/Components/CHome.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react'
 
 class CHome extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isLoggedIn: false,
+        }
+    }
+
+    handleLoginClick = () => {
+        this.setState({
+            isLoggedIn: true,
+        })
+    }
+
+    handleLogoutClick = () => {
+        this.setState({
+            isLoggedIn: false,
+        })
+    }
+
     render() {
-        return (
-            <div>
-                <h1 className="heading">Hi from home page class</h1>
-            </div>
-        )
+        if(this.state.isLoggedIn){
+            return (
+                <div>
+                    <button onClick={this.handleLogoutClick}>Logout</button>
+                </div>
+            )
+        }
+        else{
+            return (
+                <div>
+                    <button onClick={this.handleLoginClick}>Login</button>
+                </div>
+            )
+        }
     }
 }
 
