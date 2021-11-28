9ad0ea84c686fd9a1f9ef2f2f6ea57eb500ceb1c
Ajit Tupe <ajittupe2548@gmail.com>
2021-11-28 20:16:31 +0530

    Added : JSX

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <C_Home/>
-      <F_Home/>
     </div>
   );
 }
--- src/Components/c_home.js
+++ src/Components/c_home.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
+const element = React.createElement(
+    'h1',
+    {className: 'greeting',
+    id: "heading"},
+    'Hello, world!'
+  );
 class c_home extends Component {
     render() {
         return (
             <div>
-                <h1 className="heading">Hi from home page class</h1>
+                {element}
             </div>
         )
     }
