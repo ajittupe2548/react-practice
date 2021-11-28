23c7f20555022e8a686b0123a6ef99091151ad8e
Ajit Tupe <ajittupe2548@gmail.com>
2021-11-28 20:44:07 +0530

    Added : Lifecycle methods

--- src/Components/CHome.js
+++ src/Components/CHome.js
@@ -5,19 +5,42 @@ class CHome extends Component {
         super(props)
         this.state = {
             message: 'Please click',
+            count: 0,
         }
+        this.btnRef = React.createRef();
         this.handleClick = this.handleClick.bind(this);
     }
+
+    componentDidMount() {
+        console.log(`*****Output is :  => CHome => componentDidMount => componentDidMount`)
+        this.btnRef.current.addEventListener("click",this.handleClick)
+    }
+
+    shouldComponentUpdate() {
+        console.log(`*****Output is :  => CHome => shouldComponentUpdate => shouldComponentUpdate`)
+        return true;
+    }
+
+    componentDidUpdate() {
+        console.log(`*****Output is :  => CHome => componentDidUpdate => componentDidUpdate`)
+    }
+
+    componentWillUnmount() {
+        console.log(`*****Output is :  => CHome => componentWillUnmount => componentWillUnmount`)
+        this.btnRef.current.removeEventListener("click",this.handleClick)
+    }
+
     handleClick() {
         this.setState({
             message: 'Thank you for clicking',
         })
     }
     render() {
+        console.log(`*****Output is :  => CHome => render => render`)
         return (
             <div>
                 <h1 className="heading">{this.state.message}</h1>
-                <button onClick={this.handleClick}>Click</button>
+                <button ref={this.btnRef}>Click</button>
             </div>
         )
     }
