3f428690bc0bcae26ecf74fabcffd3a20d1c5b72
Ajit Tupe <ajittupe2548@gmail.com>
2021-11-28 20:30:06 +0530

    Added : State

--- src/Components/CHome.js
+++ src/Components/CHome.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
 class CHome extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            message: 'Please click',
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.setState({
+            message: 'Thank you for clicking',
+        })
+    }
     render() {
         return (
             <div>
-                <h1 className="heading">Hi from home page class</h1>
+                <h1 className="heading">{this.state.message}</h1>
+                <button onClick={this.handleClick}>Click</button>
             </div>
         )
     }
