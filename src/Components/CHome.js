9399860cc2fd82105e90a79f6e3541aabb2d4be8
Ajit Tupe <ajittupe2548@gmail.com>
2021-11-29 19:13:25 +0530

    Added : Controlled component

--- src/Components/CHome.js
+++ src/Components/CHome.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
 
 class CHome extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            nameValue: '',
+        }
+    }
+
+    handleNameChange = (event) => {
+        this.setState({
+            nameValue: event.target.value,
+        })
+    }
+
+    handleSubmit = (event) => {
+        console.log(this.state.nameValue);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div>
                 <h1 className="heading">Hi from home page class</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:&nbsp;
+                        <input type="text" value={this.state.nameValue} onChange={this.handleNameChange}/>
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
         )
     }
