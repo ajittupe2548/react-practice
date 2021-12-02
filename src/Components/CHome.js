2d47c07c4c2abf917e9982ff83592487a24ef83a
Ajit Tupe <ajittupe2548@gmail.com>
2021-12-02 19:10:41 +0530

    Added : Accessibility

--- src/Components/CHome.js
+++ src/Components/CHome.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import BlurExample from './BlurExample';
+import OuterClickExample from './OuterClickExample'
 
 class CHome extends Component {
     render() {
         return (
             <div>
                 <h1 className="heading">Hi from home page class</h1>
+                <BlurExample/>
+                <OuterClickExample />
             </div>
         )
     }
