2012cc800549695b4e76012355764a63b4b42b85
Ajit Tupe <ajittupe2548@gmail.com>
2021-12-04 14:59:21 +0530

    Added : Code splitting

--- src/Components/FHome.js
+++ src/Components/FHome.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import MyComponent from './CodeSplitting';
 
 function FHome() {
     return (
         <div>
             <h1>Hi from functional home page functional</h1>
+            <MyComponent/>
         </div>
     )
 }
---
+++ src/Components/OtherComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function OtherComponent() {
+    return (
+        <div>
+            Hi from other component
+        </div>
+    )
+}
