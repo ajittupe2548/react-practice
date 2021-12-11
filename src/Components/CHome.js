5d63c529ea3410e660fdde7d1354f690856f1e5d
Ajit Tupe <ajittupe2548@gmail.com>
2021-12-12 00:14:57 +0530

    Added : Refs

--- src/Components/CHome.js
+++ src/Components/CHome.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 
 class CHome extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: '',
+        }
+        this.inputRef = React.createRef();
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            value: e.target.value,
+        })
+    }
+
+    handleClick = () => {
+        console.log(this.state.value)
+    }
+
+    handleFocus = () => {
+        this.inputRef.current.focus();
+    }
+
     render() {
         return (
             <div>
-                <h1 className="heading">Hi from home page class</h1>
+                <input onChange={this.handleChange} ref={this.inputRef} value={this.state.value} type="text" />
+                <button onClick={this.handleFocus}>Focus</button>
+                <button onClick={this.handleClick}>Submit</button>
             </div>
         )
     }
