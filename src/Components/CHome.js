b80dccd404bb33d8cf077316469cd2a0dd1308ea
Ajit Tupe <ajittupe2548@gmail.com>
2021-12-12 00:49:36 +0530

    Added : render props

--- src/Components/CHome.js
+++ src/Components/CHome.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import ClickCounter from './ClickCounter';
+import Counter from './Counter';
+import HoverCounter from './HoverCounter';
 
 class CHome extends Component {
     render() {
         return (
             <div>
-                <h1 className="heading">Hi from home page class</h1>
+                {/* <ClickCounter />
+                <HoverCounter /> */}
+                <Counter render={(count, incrementCount) => <ClickCounter count={count} incrementCount={incrementCount}/>} />
+                <Counter render={(count, incrementCount) => <HoverCounter count={count} incrementCount={incrementCount}/>} />
             </div>
         )
     }
