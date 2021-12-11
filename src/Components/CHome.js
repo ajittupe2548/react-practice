97aa19ed05e373383cfc90803d16db4befe7b061
Ajit Tupe <ajittupe2548@gmail.com>
2021-12-11 16:45:30 +0530

    Added : Ref forwarding

--- src/Components/CHome.js
+++ src/Components/CHome.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import FRParentInput from './FRParentInput';
 class CHome extends Component {
     render() {
         return (
             <div>
                 <h1 className="heading">Hi from home page class</h1>
+                <FRParentInput />
             </div>
         )
     }
