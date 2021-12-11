e523dfb8178d09cad13ed8047f086a23301a5cbf
Ajit Tupe <ajittupe2548@gmail.com>
2021-12-11 23:34:14 +0530

    Added : Initial profiler (not right)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import CHome from './Components/CHome';
 import FHome from './Components/FHome';
+import Profiler from 'react'
 
 function App() {
+  const callback = () => {
+    console.log("ajit")
+  }
   return (
     <div className="App">
+      <Profiler id="CHome" onRender={callback}>
       <CHome/>
+      </Profiler>
       <FHome/>
     </div>
   );
