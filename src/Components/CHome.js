ac86d317c3d4d184961eccc9f4aa14fb59069a26
Ajit Tupe <ajittupe2548@gmail.com>
2021-12-11 16:52:29 +0530

    Added : Fragments

--- src/Components/CHome.js
+++ src/Components/CHome.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Table from './Table';
 
 class CHome extends Component {
     render() {
         return (
             <div>
                 <h1 className="heading">Hi from home page class</h1>
+                <Table/>
             </div>
         )
     }
