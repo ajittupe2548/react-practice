daaa8bf41ae8e67dcfdf0919f8894f6ac25810b5
Ajit Tupe <ajittupe2548@gmail.com>
2021-12-18 11:54:42 +0530

    Added : Hooks intro

--- src/Components/FHome.js
+++ src/Components/FHome.js
@@ -1,9 +1,14 @@
-import React from 'react'
+// You must import Hooks from react.
+import React, {useState} from 'react'
 
+// Hooks can only be called inside React function components.
 function FHome() {
+    // Hooks can only be called at the top level of a component.
+    const [count, setCount ] = useState(0);
     return (
         <div>
             <h1>Hi from functional home page functional</h1>
+            <button onClick={() => setCount(count + 1)}>Click {count} times</button>
         </div>
     )
 }
