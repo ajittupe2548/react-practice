8bed404f7967fa4274aea34f36223339a38c8e82
ajittupe2548 <ajittupe2548@gmail.com>
2022-03-05 17:47:22 +0530

    Used : Simple way for ref forwarding

--- src/Components/FRInput.js
+++ src/Components/FRInput.js
@@ -1,19 +1,15 @@
 import React from 'react'
 
-// function FRInput() {
-//     return (
-//         <div>
-//             <input type="text" />
-//         </div>
-//     )
-// }
-
-const FRInput = React.forwardRef((props,ref)=> {
+function FRInput({ innerRef, name }) {
     return (
         <div>
-            <input type="text" ref={ref} />
+            <input type="text" ref={innerRef} />
         </div>
     )
+}
+
+const FRInputWithRef = React.forwardRef((props,ref) => {
+    return <FRInput innerRef={ref} {...props} />
 })
 
-export default FRInput
+export default FRInputWithRef;
