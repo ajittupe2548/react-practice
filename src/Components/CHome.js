1a1bbcd9c4cfa48e620abc798d5fcc408af736ed
ajittupe2548 <ajittupe2548@gmail.com>
2022-03-16 19:34:54 +0530

    Added : Composition

--- src/Components/CHome.js
+++ src/Components/CHome.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react'
 
+function FancyBorder(props) {
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+            {props.children}
+        </div>
+    );
+}
+
+function Dialog(props) {
+    return (
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">
+                {props.title}
+            </h1>
+            <p className="Dialog-message">
+                {props.message}
+            </p>
+            {props.children}
+        </FancyBorder>
+    );
+}
+
 class CHome extends Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSignUp = this.handleSignUp.bind(this);
+        this.state = { login: '' };
+    }
+
+    handleChange(e) {
+        this.setState({ login: e.target.value });
+    }
+
+    handleSignUp() {
+        alert(`Welcome aboard, ${this.state.login}!`);
+    }
+
     render() {
         return (
-            <div>
-                <h1 className="heading">Hi from home page class</h1>
-            </div>
+            <Dialog title="Mars Exploration Program"
+                message="How should we refer to you?">
+                <input value={this.state.login}
+                    onChange={this.handleChange} />
+                <button onClick={this.handleSignUp}>
+                    Sign Me Up!
+                </button>
+            </Dialog>
         )
     }
 }
