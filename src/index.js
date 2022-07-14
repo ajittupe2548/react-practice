0e09072d7c3cc5aa752aef2125a3c434925f9a02
Ajit Tupe <ajittupe2548@gmail.com>
2022-07-14 22:33:00 +0530

    Added : Rendering

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// React Only Updates What’s Necessary
+// React DOM compares the element and its children to the previous one, and only applies the DOM updates necessary to bring the DOM to the desired state.
+
+function tick() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <h1>It is {new Date().toLocaleTimeString()}</h1>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
