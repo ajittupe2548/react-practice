f7b5f7c79ccc09a6fa34f2311ac16a363b6ba7e2
Ajit Tupe <ajittupe2548@gmail.com>
2022-07-14 22:38:49 +0530

    Added : Description

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import './App.css';
 import C_Home from './Components/c_home';
 import F_Home from './Components/f_home';
 
+// Components let you split the UI into independent, reusable pieces, and think about each piece in isolation.
+// Conceptually, components are like JavaScript functions. They accept arbitrary inputs (called “props”) and return React elements describing what should appear on the screen.
+
+// Note: Always start component names with a capital letter.
+
+// Props are Read-Only - Whether you declare a component as a function or a class, it must never modify its own props.
+
+
 function App() {
   return (
     <div className="App">
-      <C_Home name="Ajit"/>
-      <F_Home name="Prajay"/>
+      <C_Home name="SpiderMan"/>
+      <F_Home name="BatMan"/>
     </div>
   );
 }
--- src/Components/f_home.js
+++ src/Components/f_home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+// This function is a valid React component because it accepts a single “props” object argument with data and returns a React element. We call such components “function components” because they are literally JavaScript functions.
+
 function f_home(props) {
     return (
         <div>
