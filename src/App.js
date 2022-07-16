40d2470ba1f26333a0e893c96adb841ce05706a7
Ajit Tupe <ajittupe2548@gmail.com>
2022-07-16 23:38:59 +0530

    Added : Conditional rendering info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import './App.css';
 import CHome from './Components/CHome';
 import FHome from './Components/FHome';
 
+// Conditional rendering - Conditional rendering in React works the same way conditions work in JavaScript.
+
+// 1) Element Variables - You can use variables to store elements. This can help you conditionally render a part of the component while the rest of the output doesn’t change.
+
+// 2) Inline If with Logical && Operator - You may embed expressions in JSX by wrapping them in curly braces. This includes the JavaScript logical && operator.
+
+// 3) Inline If-Else with Conditional Operator - Another method for conditionally rendering elements inline is to use the JavaScript conditional operator condition ? true : false.
+
 function App() {
   return (
     <div className="App">
