598b408940c730530feee5212534dc353c0cb390
Ajit Tupe <ajittupe2548@gmail.com>
2022-07-16 23:34:13 +0530

    Added : Events info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import './App.css';
 import CHome from './Components/CHome';
 import FHome from './Components/FHome';
 
+// Handling Events - Handling events with React elements is very similar to handling events on DOM elements
+// - React events are named using camelCase, rather than lowercase.
+// - With JSX you pass a function as the event handler, rather than a string.
+
+// HTML
+// <button onclick="activateLasers()">
+//   Activate Lasers
+// </button>
+
+// React
+// <button onClick={activateLasers}>
+//   Activate Lasers
+// </button>
+
 function App() {
   return (
     <div className="App">
