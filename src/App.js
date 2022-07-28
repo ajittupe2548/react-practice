72b22a4eb447f4142ae6d7c074c791a417edf218
Ajit Tupe <ajittupe2548@gmail.com>
2022-07-28 14:57:10 +0530

    Added : Forms info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 import CHome from './Components/CHome';
 import FHome from './Components/FHome';
 
+// Forms - HTML form elements work a bit differently from other DOM elements in React, because form elements naturally keep some internal state.
+
+// Controlled Components - In HTML, form elements such as <input>, <textarea>, and <select> typically maintain their own state and update it based on user input. In React, mutable state is typically kept in the state property of components, and only updated with setState().
+
+// Alternatives to Controlled Components - It can sometimes be tedious to use controlled components, because you need to write an event handler for every way your data can change and pipe all of the input state through a React component. This can become particularly annoying when you are converting a preexisting codebase to React, or integrating a React application with a non-React library. In these situations, you might want to check out uncontrolled components(https://reactjs.org/docs/uncontrolled-components.html), an alternative technique for implementing input forms.
+
 function App() {
   return (
     <div className="App">
--- src/Components/CHome.js
+++ src/Components/CHome.js
@@ -6,17 +6,22 @@ class CHome extends Component {
 
         this.state = {
             nameValue: '',
+            essayValue: 'Started essay with few words!',
+            selectValue: 'coconut'
         }
     }
 
-    handleNameChange = (event) => {
+    handleChange = (event) => {
+        const target = event.target;
+        const value = event.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
         this.setState({
-            nameValue: event.target.value,
+            [name]: value,
         })
     }
 
     handleSubmit = (event) => {
-        console.log(this.state.nameValue);
+        console.log(this.state.nameValue, this.state.essayValue, this.state.selectValue);
         event.preventDefault();
     }
 
@@ -25,10 +30,26 @@ class CHome extends Component {
             <div>
                 <h1 className="heading">Hi from home page class</h1>
                 <form onSubmit={this.handleSubmit}>
+                    {/* The input Tag */}
                     <label>
                         Name:&nbsp;
-                        <input type="text" value={this.state.nameValue} onChange={this.handleNameChange}/>
+                        <input name="nameValue" type="text" value={this.state.nameValue} onChange={this.handleChange}/>
                     </label>
+                    {/* The textarea Tag */}
+                    <label>
+                        Essay:
+                        <textarea name="essayValue" value={this.state.essayValue} onChange={this.handleChange} />
+                    </label>
+                    {/* The select Tag */}
+                    <label>
+                        Pick your favorite flavor:
+                        <select name="selectValue" value={this.state.selectValue} onChange={this.handleChange}>
+                            <option value="grapefruit">Grapefruit</option>
+                            <option value="lime">Lime</option>
+                            <option value="coconut">Coconut</option>
+                            <option value="mango">Mango</option>
+                        </select>
+                        </label>
                     <input type="submit" value="Submit" />
                 </form>
             </div>
