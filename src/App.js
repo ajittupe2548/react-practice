93672a37c59b7737927743ae314e6575a4ff5da4
Ajit Tupe <ajittupe2548@gmail.com>
2022-08-04 16:30:33 +0530

    Added : Lifting state up info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import CHome from './Components/CHome';
 import FHome from './Components/FHome';
 
+// Lifting State Up - Often, several components need to reflect the same changing data. We recommend lifting the shared state up to their closest common ancestor.
+
+// There should be a single “source of truth” for any data that changes in a React application. Usually, the state is first added to the component that needs it for rendering. Then, if other components also need it, you can lift it up to their closest common ancestor. Instead of trying to sync the state between different components, you should rely on the top-down data flow.
+
 function App() {
   return (
     <div className="App">
