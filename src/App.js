7d3ad3a1ee153a7dbc44b8de769c5402676a9555
Ajit Tupe <ajittupe2548@gmail.com>
2022-09-15 09:23:17 +0530

    Added : Fragments info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React from 'react';
 import './App.css';
 import CHome from './Components/CHome';
 import FHome from './Components/FHome';
+
+// Fragments - A common pattern in React is for a component to return multiple elements. Fragments let you group a list of children without adding extra nodes to the DOM.
+
+// Keyed Fragments - Fragments declared with the explicit <React.Fragment> syntax may have keys. A use case for this is mapping a collection to an array of fragments — for example, to create a description list:
+// key is the only attribute that can be passed to Fragment.
+
+function Glossary(props) {
+  return (
+    <dl>
+      {props.items.map(item => (
+        // Without the `key`, React will fire a key warning
+        <React.Fragment key={item.id}>
+          <dt>{item.term}</dt>
+          <dd>{item.description}</dd>
+        </React.Fragment>
+      ))}
+    </dl>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <CHome/>
+      <Glossary items={[{id:1, term: "Coffee", description: "Black hot drink"}, {id:2, term: "Milk", description: "White cold drink"}]}/>
     </div>
   );
 }
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Columns from "./Columns";
 
+// Motivation - <Columns /> would need to return multiple <td> elements in order for the rendered HTML to be valid. If a parent div was used inside the render() of <Columns />, then the resulting HTML will be invalid.
+// Fragments solve this problem.
+
 class Table extends React.Component {
   render() {
     return (
