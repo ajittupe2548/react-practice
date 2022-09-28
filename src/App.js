58049e1e2922d1405cc843e52388c3b9707ec7fa
Ajit Tupe <ajittupe2548@gmail.com>
2022-09-28 09:37:41 +0530

    Added : Info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import CHome from './Components/CHome';
 
+// React can be used in any web application. It can be embedded in other applications and, with a little care, other applications can be embedded in React.
+
+// Integrating with DOM Manipulation Plugins - React is unaware of changes made to the DOM outside of React. It determines updates based on its own internal representation, and if the same DOM nodes are manipulated by another library, React gets confused and has no way to recover.
+
 function App() {
   return (
     <div className="App">
