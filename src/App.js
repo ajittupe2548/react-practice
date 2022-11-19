3e3cfd6f4b90f51cb73670698e3fa20b866ef409
Ajit Tupe <ajittupe2548@gmail.com>
2022-11-19 20:04:26 +0530

    Added : Web component info

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import AxiosPost from './Components/AxiosPost';
-import CHome from './Components/CHome';
-import FHome from './Components/FHome';
+
+// React and Web Components are built to solve different problems. Web Components provide strong encapsulation for reusable components, while React provides a declarative library that keeps the DOM in sync with your data. The two goals are complementary. As a developer, you are free to use React in your Web Components, or to use Web Components in React, or both.
+// Most people who use React don’t use Web Components, but you may want to, especially if you are using third-party UI components that are written using Web Components.
 
 function App() {
   return (
     <div className="App">
-      {/* <CHome/> */}
       <AxiosPost />
     </div>
   );
