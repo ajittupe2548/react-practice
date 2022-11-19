31948c9d01c6d86a8e47d4519681fa10ac7e7e48
Ajit Tupe <ajittupe2548@gmail.com>
2022-11-19 19:04:36 +0530

    Added : Strict mode info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
 import C_Home from './Components/c_home';
-import F_Home from './Components/f_home';
+
+// StrictMode is a tool for highlighting potential problems in an application. Like Fragment, StrictMode does not render any visible UI. It activates additional checks and warnings for its descendants.
+// Note: Strict mode checks are run in development mode only; they do not impact the production build.
+
+// StrictMode currently helps with:
+// Identifying components with unsafe lifecycles
+// Warning about legacy string ref API usage
+// Warning about deprecated findDOMNode usage
+// Detecting unexpected side effects
+// Detecting legacy context API
+// Ensuring reusable state
 
 function App() {
   return (
