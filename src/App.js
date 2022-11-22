1f3941774a93a0f4600115613e14e535449d05eb
Ajit Tupe <ajittupe2548@gmail.com>
2022-11-22 18:48:51 +0530

    Added : dom-server

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import CHome from './Components/CHome';
 import FHome from './Components/FHome';
 
+// The ReactDOMServer object enables you to render components to static markup. Typically, it’s used on a Node server.
+// For more info : https://reactjs.org/docs/react-dom-server.html
+
 function App() {
   return (
     <div className="App">
