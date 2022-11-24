13880ef2c52995d1591e6d71368fc6df3f6551c2
Ajit Tupe <ajittupe2548@gmail.com>
2022-11-24 22:15:53 +0530

    Added : Synthetic events

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import CHome from './Components/CHome';
 import FHome from './Components/FHome';
 
+// SyntheticEvent - This reference guide documents the SyntheticEvent wrapper that forms part of React’s Event System.
+// More info - https://reactjs.org/docs/events.html
+
 function App() {
   return (
     <div className="App">
