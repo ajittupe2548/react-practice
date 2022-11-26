ec9037c2918ae3e34014ac97cc1e43ec83cab756
Ajit Tupe <ajittupe2548@gmail.com>
2022-11-26 16:16:09 +0530

    Added : Js env requirements

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import './App.css';
 import CHome from './Components/CHome';
 import FHome from './Components/FHome';
 
+// JavaScript Environment Requirements - React 18 supports all modern browsers (Edge, Firefox, Chrome, Safari, etc).
+// If you support older browsers and devices such as Internet Explorer which do not provide modern browser features natively or have non-compliant implementations, consider including a global polyfill in your bundled application.
+
+// Here is a list of the modern features React 18 uses:
+// - Promise
+// - Symbol
+// - Object.assign
+// The correct polyfill for these features depend on your environment. For many users, you can configure your Browserlist settings. For others, you may need to import polyfills like core-js directly.
+
 function App() {
   return (
     <div className="App">
