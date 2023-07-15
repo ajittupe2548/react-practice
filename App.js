5d566fbeb05b442b651cc9b46450ce4b92f8e6ea
Ajit Tupe <ajittupe2548@gmail.com>
2023-07-15 23:46:05 +0530

    Added : Parcel

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
+
+const reactHeading1 = React.createElement("h2", {
+    className: "react-heading",
+    key: 1,
+}, "Hello World 1 from React!");
+
+const reactHeading2 = React.createElement("h2", {
+    className: "react-heading",
+    key: 2,
+}, "Hello World 2 from React!");
+
+const reactHeadingContainer = React.createElement("div", { className: "react-heading-container", }, [reactHeading1, reactHeading2]);
+
+reactRoot.render(reactHeadingContainer);
