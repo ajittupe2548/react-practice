556a4281dcfd279eb832f717da4c55ca8f2e0b24
Ajit Tupe <ajittupe2548@gmail.com>
2023-07-25 22:45:24 +0530

    Update Assignment.js

--- Assignment.js
+++ Assignment.js
@@ -1,3 +1,23 @@
 /*
+What is the difference between Named Export, Default export and * as export?
+Named Export: Used to export multiple items explicitly and imported using their specific names.
+Default Export: Used to export a single item per module and can be imported with any custom name.
+* as Export (Namespace Import): Used to import all named exports from a module and access them as properties of an object.
 
+
+What is the importance of constants.js file?
+The constants.js file is used to store constant values that remain the same throughout the application's lifecycle.
+
+
+What are React Hooks?
+React Hooks are a feature introduced in React version 16.8 to allow functional components to have stateful behavior and access React features that were previously available only in class components. Eg. useState, useEffect, useRef etc.
+
+
+Why do we need a useState Hook?
+useState hooks is needed to maintain a state in react component. The useState Hook in React is essential because it allows functional components to have local state.
+
+
+What is one way binding and two way binding in react?
+One-way binding allows data to flow in a single direction, typically from parent to child components, ensuring that data changes in the data source are reflected in the UI.
+Two-way binding enables data to flow in both directions, allowing changes in the UI to update the data source and vice versa, often achieved using controlled components.
 */
