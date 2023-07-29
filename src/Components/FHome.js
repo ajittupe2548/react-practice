4e20ff7fb4db8cb317cbd484539dec1179eeeb16
Ajit Tupe <ajittupe2548@gmail.com>
2023-07-29 21:48:10 +0530

    Update FHome.js

--- src/Components/FHome.js
+++ src/Components/FHome.js
@@ -1,4 +1,6 @@
-import React, {useEffect, useRef} from 'react'
+import React, { useEffect, useRef } from 'react'
+
+// useRef hook is used to create a mutable reference to a DOM element or any other value that persists across renders. Unlike useState, updating the value stored in a useRef does not trigger a re-render of the component. It is often used to interact with the DOM directly or to hold onto a value that you want to persist between renders without causing a re-render.
 
 function FHome() {
 
@@ -6,10 +8,10 @@ function FHome() {
 
     useEffect(() => {
         inputRef.current.focus();
-    },[])
+    }, [])
     return (
         <div>
-            <input ref={inputRef} type="text"/>
+            <input ref={inputRef} type="text" />
         </div>
     )
 }
