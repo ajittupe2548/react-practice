c3fd89b2d97a85dcdcf68fb4160a9f193467c29b
Ajit Tupe <ajittupe2548@gmail.com>
2023-07-29 21:39:20 +0530

    useEffect

--- src/Components/UseEffectWithDependency.js
+++ src/Components/UseEffectWithDependency.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 
+// This hook enables you to perform side effects in functional components. It replaces lifecycle methods like componentDidMount, componentDidUpdate, and componentWillUnmount.
+// useEffect callback cannot be declared as an async function directly. However, you can use an immediately-invoked function expression (IIFE) inside the useEffect to work with async functions.
+
 function UseEffectWithDependency() {
     const [count, setCount] = useState(0);
 
