9c5aced67628cd2c02c1ec5d7672f679061b66fe
Ajit Tupe <ajittupe2548@gmail.com>
2023-08-02 23:19:19 +0530

    React router dom

--- App.js
+++ App.js
@@ -1,8 +1,111 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider, useRouteError, Link, Outlet, useParams } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 
 const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
 
-const reactHeadingContainer = React.createElement("h2", {}, "Hi!");
+const AppLayout = () => {
+    console.log(React.useState())
+    return (
+        <>
+            <Link to='/'>
+                <button>Home</button>
+            </Link>
+            <Link to='/about'>
+                <button>About</button>
+            </Link>
+            <Link to='/about/us'>
+                <button>About Us</button>
+            </Link>
+            <Outlet />
+            <h2>Footer</h2>
+        </>
+    );
+}
 
-reactRoot.render(reactHeadingContainer);
+const Home = () => {
+    return (
+        <>
+            <h2>Home</h2>
+        </>
+    );
+};
+
+const About = () => {
+    return (
+        <>
+            <h2>About</h2>
+        </>
+    );
+};
+
+const DynamicAbout = () => {
+    const { id } = useParams();
+    return (
+        <>
+            <h2>Dynamic About Id : {id}</h2>
+        </>
+    );
+};
+
+const AboutUs = () => {
+    return (
+        <>
+            <h2>About Us</h2>
+        </>
+    );
+};
+
+const HomeError = () => {
+    const err = useRouteError();
+    return (
+        <>
+            <h2>Home Error</h2>
+            <h3>{err.status} : {err.statusText}</h3>
+        </>
+    );
+}
+
+const AboutError = () => {
+    return (
+        <h2>About Error</h2>
+    );
+}
+
+const AboutUsError = () => {
+    return (
+        <h2>About Us Error</h2>
+    );
+}
+
+const appRoutes = createBrowserRouter([
+    {
+        path: '/',
+        element: <AppLayout />,
+        errorElement: <HomeError />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+                errorElement: <HomeError />,
+            },
+            {
+                path: '/about',
+                element: <About />,
+                errorElement: <AboutError />,
+            },
+            {
+                path: '/about/:id',
+                element: <DynamicAbout />,
+                errorElement: <AboutError />,
+            },
+            {
+                path: '/about/us',
+                element: <AboutUs />,
+                errorElement: <AboutUsError />,
+            },
+        ]
+    }
+])
+
+reactRoot.render(<RouterProvider router={appRoutes} />);
--- Assignment.js
+++ Assignment.js
@@ -1,3 +1,32 @@
 /*
+What are various ways to add images into our App?
+1) Importing images directly
+2) Using Image URLs
+3) Using public folder
+4) Using CSS background-image
 
+
+What would happen if we do console.log(useState())?
+It will log `[undefined, ƒ]`
+
+
+How will useEffect behave if we don't add a dependency array?
+Callback function of useEffect will execute for every render.
+
+
+What is SPA (Single Page Application)?
+It is a web application or website that functions on a single web page by dynamically updating the content on the page instead of loading entire new pages from the server. In traditional web applications, clicking on a link or submitting a form would typically result in the browser requesting a new page from the server. However, in SPAs, the initial HTML, CSS, and JavaScript are loaded once, and subsequent interactions or data updates are handled asynchronously through APIs or AJAX calls.
+
+
+What is difference between Client Side Routing and Server Side Routing?
+- Client-Side Routing:
+1) Routing logic and navigation handled on the user's web browser.
+2) JavaScript frameworks like React Router or Angular Router used.
+3) Clicking links updates content and URL without server requests.
+4) Faster subsequent page loads, server mainly provides data and APIs.
+- Server-Side Routing:
+1) Routing logic and navigation handled by the web server.
+2) Each user action requires a new request to the server.
+3) Full page reloads for navigation.
+4) Slower initial load times, server generates and serves HTML pages.
 */
