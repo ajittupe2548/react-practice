3f149e0b4c6da7a8a50bc82713d1616b573decc1
Ajit Tupe <ajittupe2548@gmail.com>
2023-08-14 00:47:15 +0530

    useContext, lifting state up

--- App.js
+++ App.js
@@ -1,53 +1,84 @@
-import React from 'react';
+import React, { Component, useContext, useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import NameContext from './NameContext';
 
 const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
 
-const Parent = ({name}) => {
+const Parent = () => {
+    const [name, setName] = useState("Abc");
     return (
         <>
-            <Child1 name={name} />
-            <Child2 name={name} />
+            <NameContext.Provider value={{ name: name, setName: (value) => setName(value) }}>
+                <Child1 />
+            </NameContext.Provider>
+            <Child2 />
         </>
     )
 }
 
-const Child1 = ({name}) => {
+const Child1 = () => {
     return (
         <>
-            <GrandChild1 name={name} />
+            <GrandChild1 />
         </>
     )
 }
 
-const GrandChild1 = ({name}) => {
+const GrandChild1 = () => {
+    const { name, setName } = useContext(NameContext);
+    const [value, setValue] = useState("");
     return (
         <>
             <p>GrandChild 1 : {name}</p>
+            <input type="text" value={value} onChange={(e) => { setValue(e.target.value); setName(e.target.value); }} />
         </>
     )
 }
 
-const Child2 = ({name}) => {
+const Child2 = () => {
     return (
         <>
-            <GrandChild2 name={name} />
+            <GrandChild2 />
         </>
     )
 }
 
-const GrandChild2 = ({name}) => {
+class GrandChild2 extends Component {
+    render() {
+        return (
+            <NameContext.Consumer>
+                {
+                    name => <p>GrandChild 2 : {name.name}</p>
+                }
+            </NameContext.Consumer>
+        )
+    }
+}
+
+const ExpandCollapse = ({ title, description, isActive, setActiveIndex, index }) => {
     return (
         <>
-            <p>GrandChild 2 : {name}</p>
+            <p>{title}</p>
+            {
+                isActive && <p>{description}</p>
+            }
+            <button onClick={() => setActiveIndex(index)}>{isActive ? 'Hide' : 'Show'}</button>
         </>
+    )
+}
+
+const StateLifting = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    return (
+        Array.from({ length: 5 }, (i, index) => <ExpandCollapse key={index} index={index} isActive={index === activeIndex} setActiveIndex={(index) => setActiveIndex(index === activeIndex ? null : index)} title={`Title ${index + 1}`} description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries" />)
     )
 }
 
 const App = () => {
     return (
         <>
-            <Parent name="John" />
+            <Parent />
+            <StateLifting />
         </>
     )
 }
--- Assignment.js
+++ Assignment.js
@@ -1,4 +1,17 @@
 /*
 What is prop drilling?
 Prop drilling occurs when a parent component passes data down to its children and then those children pass the same data down to their own children. This process can continue indefinitely.
+
+
+What is Context Provider and Context Consumer?
+A Context Provider is a component that encapsulates a certain piece of data that needs to be shared across multiple components. It essentially acts as a data source that can be accessed by its child components regardless of their nesting depth. This helps avoid "prop drilling," where props are passed through multiple layers of components.
+A Context Consumer is a component that subscribes to the data provided by a Context Provider. It allows components deep within the component tree to access the data without having to pass it through intermediate components using props.
+
+
+If you don’t pass a value to the provider does it take the default value?
+Yes, in many programming contexts, if you don't pass a value to a function or a method that has a default parameter, the default value will be used.
+
+
+What is lifting the state up?
+Lifting state up in React refers to a technique used to manage and share state between multiple components by moving the state to a common ancestor component. This is done to maintain a single source of truth for the shared data and ensure that changes to the state are consistent across the components.
 */
