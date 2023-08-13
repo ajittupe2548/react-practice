4b2ba34d5f5876180912f14380d53f6ab83fd4f8
Ajit Tupe <ajittupe2548@gmail.com>
2023-08-13 19:42:35 +0530

    prop drilling

--- App.js
+++ App.js
@@ -3,6 +3,53 @@ import ReactDOM from 'react-dom/client';
 
 const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
 
-const reactHeadingContainer = React.createElement("h2", {}, "Hi!");
+const Parent = ({name}) => {
+    return (
+        <>
+            <Child1 name={name} />
+            <Child2 name={name} />
+        </>
+    )
+}
 
-reactRoot.render(reactHeadingContainer);
+const Child1 = ({name}) => {
+    return (
+        <>
+            <GrandChild1 name={name} />
+        </>
+    )
+}
+
+const GrandChild1 = ({name}) => {
+    return (
+        <>
+            <p>GrandChild 1 : {name}</p>
+        </>
+    )
+}
+
+const Child2 = ({name}) => {
+    return (
+        <>
+            <GrandChild2 name={name} />
+        </>
+    )
+}
+
+const GrandChild2 = ({name}) => {
+    return (
+        <>
+            <p>GrandChild 2 : {name}</p>
+        </>
+    )
+}
+
+const App = () => {
+    return (
+        <>
+            <Parent name="John" />
+        </>
+    )
+}
+
+reactRoot.render(<App />);
--- Assignment.js
+++ Assignment.js
@@ -1,3 +1,4 @@
 /*
-
+What is prop drilling?
+Prop drilling occurs when a parent component passes data down to its children and then those children pass the same data down to their own children. This process can continue indefinitely.
 */
