fb5816447f5b8c84c23371cc96a78a1febae286d
Ajit Tupe <ajittupe2548@gmail.com>
2023-08-14 12:36:38 +0530

    Personal Branding

--- Assignment.js
+++ Assignment.js
@@ -1,3 +1,5 @@
 /*
-
+What is Personal Branding?
+Personal branding is the conscious and intentional effort to create and influence public perception of an individual by positioning them as an authority in their industry, elevating their credibility, and differentiating themselves from the competition, to ultimately advance their career, widen their circle of influence, and have a larger impact.
+Personal branding refers to the practice of creating and managing a distinct identity and image for oneself, often used to establish a unique and recognizable presence in the professional world. It involves showcasing your skills, expertise, values, and personality in a way that sets you apart from others and helps you achieve your goals, whether they are career-related, entrepreneurial, or personal.
 */
