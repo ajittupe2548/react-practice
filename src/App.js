9768390b2d680ad40011a9236e1bfd175e484fc1
Ajit Tupe <ajittupe2548@gmail.com>
2024-07-21 23:48:03 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import './App.css';
+import { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+}
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
+      <Counter />
     </div>
   );
 }
