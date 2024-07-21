9666b1f2b79d8e460ff6f06ad7a60c95f940eb0b
Ajit Tupe <ajittupe2548@gmail.com>
2024-07-21 23:41:14 +0530

    Removed: Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import CHome from './Components/CHome';
-import FHome from './Components/FHome';
 
 function App() {
   return (
     <div className="App">
-      <CHome/>
-      <FHome/>
     </div>
   );
 }
