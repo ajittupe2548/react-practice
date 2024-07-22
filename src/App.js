1a32a2a91415ec86fc4e2d1c810195b60be76887
Ajit Tupe <ajittupe2548@gmail.com>
2024-07-22 23:45:26 +0530

    useContext

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import './App.css';
+import { createContext, memo, useContext, useEffect, useState } from 'react';
 
-function App() {
+const ThemeContext = createContext(null);
+
+export default function MyApp() {
+  const [value, setValue] = useState("Dark");
+  const [value2, setValue2] = useState("Dark");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setValue2("Light")
+    }, 2000);
+  }, []);
+
   return (
-    <div className="App">
-    </div>
+    <ThemeContext.Provider value={value}>
+      <Form />
+    </ThemeContext.Provider>
+  )
+}
+
+const Form = () => {
+  console.log(`*****Output is :  => Form => Form:`);
+  return (
+    <Panel title="Welcome">
+      <Button>Sign up</Button>
+      <Button>Log in</Button>
+    </Panel>
   );
 }
 
-export default App;
+const Panel = ({ title, children }) => {
+  console.log(`*****Output is :  => Panel => Panel:`);
+  const theme = useContext(ThemeContext);
+  return (
+    <section>
+      <h1>{title} - {theme}</h1>
+      {children}
+    </section>
+  )
+}
+
+const Button = memo(({ children }) => {
+  console.log(`*****Output is :  => Button => Button:`);
+  const theme = useContext(ThemeContext);
+  return (
+    <button>
+      {children} - {theme}
+    </button>
+  );
+});
