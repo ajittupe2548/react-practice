274ad970b55ade70baf7bf720b005902c8b674d2
Ajit Tupe <ajittupe2548@gmail.com>
2024-07-23 15:41:06 +0530

    Merge branch 'master' into hooks-5-useRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import FHome from './Components/FHome';
-import UseRefWithInterval from './Components/UseRefWithInterval'
 
 function App() {
   return (
     <div className="App">
-      <FHome/>
-      <UseRefWithInterval/>
     </div>
   );
 }
