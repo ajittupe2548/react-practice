f658162d44b400bdba90a5a8cfb088d28203e9b6
Ajit Tupe <ajittupe2548@gmail.com>
2024-07-27 23:38:56 +0530

    Added: UI runtime

--- src/index.js
+++ src/index.js
@@ -1,15 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// root.render(
+//   <App />
+// );
+
+/* Reconciliation start */
+// const App = () => {
+//   const [className, setClassName] = useState('blue');
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setClassName('red');
+//     }, 1000);
+//   })
+//   return (
+//     <>
+//       <h1>Heading</h1>
+//       <button className={className}>{className}</button>
+//     </>
+//   )
+// }
+
+// root.render(
+//   <>
+//     <h1>Heading</h1>
+//     <button className="blue">blue</button>
+//   </>
+// );
+
+// setTimeout(() => {
+//   root.render(
+//     <>
+//       <button className="red">red</button>
+//       <h1>Heading</h1>
+//     </>
+//   );
+// }, 1000);
+/* Reconciliation end */
+
+/* Lists */
+/* https://codepen.io/gopinav/pen/gQpepq */
+const App = () => {
+  const [list, setList] = useState([
+    { id: 100 },
+    { id: 101 },
+    { id: 103 },
+    { id: 104 },
+  ]);
+
+  const handleDescClick = () => {
+    let revList = list.sort((a, b) => {
+      return b.id - a.id;
+    });
+    setList([...revList]);
+  };
+
+  const handleAscClick = () => {
+    let revList = list.sort((a, b) => {
+      return a.id - b.id;
+    });
+    setList([...revList]);
+  };
+
+  return (
+    <>
+      <h1>List and keys</h1>
+      <button onClick={handleDescClick}>Descending</button>
+      <button onClick={handleAscClick}>Ascending</button>
+      {list.map((li, index) => {
+        return (
+          <p key={li.id}>
+            <span>{index}</span> | <span>{li.id}</span> |{' '}
+            <input defaultValue={index} />
+          </p>
+        );
+      })}
+    </>
+  );
+};
+root.render(<App />);
