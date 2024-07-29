0498e512bc5e70b0d19ef7a49bec20be73f4d25b
cw-ajit <ajit@carwale.com>
2024-07-29 13:14:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ class Example extends Component {
         super(); // Use props argument here to access `this` in constructor
         console.log("this.props in constructor", this.props);
         this.state = {
-            isOn: props
-                .isOn /* If super(props) is used, then we can also use this.props.isOn here. Methods other than the constructor don't need super(props). */,
+            isOn: props.isOn /* If super(props) is used, then we can also use this.props.isOn here. Methods other than the constructor don't need super(props). */,
         };
     }
 
     render() {
-      console.log("this.props in render", this.props);
+        console.log("this.props in render", this.props);
         return <>Class Component {this.state.isOn ? "ON" : "OFF"}</>;
     }
 }
