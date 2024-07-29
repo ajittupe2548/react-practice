3e240d644ca0a83b2c0c6f3830551c94415d2c92
cw-ajit <ajit@carwale.com>
2024-07-29 13:14:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class Example extends Component {
+    constructor(props) {
+        // console.log(this);  // 🔴 Can’t use `this` yet
+        super(); // Use props argument here to access `this` in constructor
+        console.log("this.props in constructor", this.props);
+        this.state = {
+            isOn: props
+                .isOn /* If super(props) is used, then we can also use this.props.isOn here. Methods other than the constructor don't need super(props). */,
+        };
+    }
+
+    render() {
+      console.log("this.props in render", this.props);
+        return <>Class Component {this.state.isOn ? "ON" : "OFF"}</>;
+    }
+}
+
+class App extends Example {
+    render() {
+        return (
+            <>
+                <Example isOn={true} />
+            </>
+        );
+    }
 }
 
 export default App;
