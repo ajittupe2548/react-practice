ab63c6b56543bf136f0975b1c7c0a97d073b9916
cw-ajit <ajit@carwale.com>
2024-07-30 13:15:19 +0530

    Added: Custom hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import "./App.css";
+import useFetch from "./useFetchData";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const { data, loading, error } = useFetch(
+        "https://jsonplaceholder.typicode.com/users"
+    );
+
+    if (loading) return "Loading...";
+    if (error) return "Error!";
+
+    return <div className="App">{JSON.stringify(data)}</div>;
 }
 
 export default App;
---
+++ src/useFetchData.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            setLoading(false);
+            try {
+                const res = await fetch(url);
+                if (!res.ok) {
+                    throw new Error("Network Error");
+                }
+                const data = await res.json();
+                setData(data);
+            } catch (err) {
+                setError(err);
+            }
+            setLoading(false);
+        })();
+    }, [url]);
+
+    return { data, loading, error };
+};
+
+export default useFetch;
