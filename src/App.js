2163bf42dbe1fc9d3fd8fe7bfdba489d9a869a78
cw-ajit <ajit@carwale.com>
2024-08-01 17:35:17 +0530

    Added: Basic form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import "./App.css";
-import React from "react";
-import { RenderFunctionComponentV2 } from "./useState";
+import React, { useState, useEffect } from "react";
+import { RenderFunctionComponentV2 } from "./useState";   /* Even if we are not using this component don't remove import */
+
+function Form() {
+    const [name, setName] = useState("Mary"); // State variable 1
+    const [surname, setSurname] = useState("Poppins"); // State variable 2
+    const [width, setWidth] = useState(window.innerWidth); // State variable 3
+
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    });
+
+    function handleNameChange(e) {
+        setName(e.target.value);
+    }
+
+    function handleSurnameChange(e) {
+        setSurname(e.target.value);
+    }
+
+    return (
+        <>
+            <input value={name} onChange={handleNameChange} />
+            <input value={surname} onChange={handleSurnameChange} />
+            <p>
+                Hello, {name} {surname}
+            </p>
+            <p>Window width: {width}</p>
+        </>
+    );
+}
 
 function App() {
   return (
     <div className="App">
+      <Form />
     </div>
   );
 }
