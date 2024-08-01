c5663fb3281ee9038a563bfeeec211ef3c91c21f
Ajit Tupe <ajittupe2548@gmail.com>
2024-08-01 22:12:09 +0530

    Init: Theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Link to='/'>Home</Link>
+      <Link to='/blogs'>Blogs</Link>
+      <Link to='/contact'>Contact</Link>
+      <Routes>
+        <Route path='' element='home' />
+        <Route path='/blogs' element='Blogs' />
+        <Route path='/contact' element='Contact' />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
