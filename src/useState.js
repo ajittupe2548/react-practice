95af3ededc230d5dc1bc0269e106d05324b218ca
cw-ajit <ajit@carwale.com>
2024-08-01 16:37:40 +0530

    Added: Order issue

--- src/useState.js
+++ src/useState.js
@@ -1,3 +1,5 @@
+/* https://medium.com/@ryardley/react-hooks-not-magic-just-arrays-cd4f1857236e */
+
 let state = [];
 let setters = [];
 let cursor = 0;
@@ -25,6 +27,12 @@ export function useStateCustom(initVal) {
 
 // Our component code that uses hooks
 export function RenderFunctionComponent() {
+    let init;
+    /* If we uncomment the code below, then for the first render, we have three states (init, firstName, lastName) and three setter functions (()=>{}, setFirstName, setLastName). However, for subsequent renders, we will only have two states (firstName, lastName) and two setters (setFirstName, setLastName). But our global state array will still have three states (init, firstName, lastName) and three setters (()=>{}, setFirstName, setLastName). So, for render 2 and onwards, our firstName setter will get attached to the init setter in the global state, and the other setters will shift by one. */
+    // if(renderCount === 0) {
+    //     [init] = useStateCustom("Init");
+    // }
+
     const [firstName, setFirstName] = useStateCustom("Rudi"); // cursor: 0
     const [lastName, setLastName] = useStateCustom("Yardley"); // cursor: 1
 
