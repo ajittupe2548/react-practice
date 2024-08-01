fa7880d338c24d1656ed89e5632fe6fcd57b0d33
Ajit Tupe <ajittupe2548@gmail.com>
2024-08-02 00:04:24 +0530

    Used context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, createContext, useContext } from 'react';
 import './App.css';
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 
-function App() {
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
 
   useEffect(() => {
@@ -10,26 +12,43 @@ function App() {
     localStorage.setItem('theme', theme);
   }, [theme]);
 
-  const handleThemeClick = () => {
+  const toggleTheme = () => {
     setTheme(prev => prev === 'light' ? 'dark' : 'light');
   }
 
   return (
-    <BrowserRouter>
-      <div className='header'>
-        <div className='navList'>
-          <Link to='/' className='navItem'>Home</Link>
-          <Link to='/blogs' className='navItem'>Blogs</Link>
-          <Link to='/contact' className='navItem'>Contact</Link>
-        </div>
-        <div className='iconWrapper' onClick={handleThemeClick}>{theme}</div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+const Header = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  return (
+    <div className='header'>
+      <div className='navList'>
+        <Link to='/' className='navItem'>Home</Link>
+        <Link to='/blogs' className='navItem'>Blogs</Link>
+        <Link to='/contact' className='navItem'>Contact</Link>
       </div>
-      <Routes>
-        <Route path='' element='Home' />
-        <Route path='/blogs' element='Blogs' />
-        <Route path='/contact' element='Contact' />
-      </Routes>
-    </BrowserRouter >
+      <div className='iconWrapper' onClick={toggleTheme}>{theme}</div>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <ThemeProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='' element='Home' />
+          <Route path='/blogs' element='Blogs' />
+          <Route path='/contact' element='Contact' />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
