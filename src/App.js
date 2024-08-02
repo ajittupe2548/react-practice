716c6621606a27fdb7bdad7e17a3e7b2bc79e91b
Ajit Tupe <ajittupe2548@gmail.com>
2024-08-02 23:44:16 +0530

    Init: Breadcrumb

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+const Home = () => <div>Home</div>
+const Listing = () => <div>Listing</div>
+const Details = () => <div>Details</div>
+
+const Header = () => {
+  return (
+    <div className='navBar'>
+      <Link to='/' className='navItem'>Home</Link>
+      <Link to='/listing' className='navItem'>Listing</Link>
+      <Link to='/listing/details' className='navItem'>Details</Link>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/listing' element={<Listing />} />
+        <Route path='/listing/details' element={<Details />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
