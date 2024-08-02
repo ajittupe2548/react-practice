050bb9d217229c1e4f85bf9210a777bbf251945c
cw-ajit <ajit@carwale.com>
2024-08-02 15:22:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import { RenderFunctionComponentV2 } from "./useState";   /* Even if we are not using this component don't remove import */
+import { RenderFunctionComponentV2 } from "./useState"; /* Even if we are not using this component don't remove import */
+
+/* https://medium.com/7shifts-rd/why-does-hook-call-order-matter-the-rules-of-react-hooks-explained-55d1dc0a5eff */
 
 function Form() {
     const [name, setName] = useState("Mary"); // State variable 1
@@ -34,11 +36,11 @@ function Form() {
 }
 
 function App() {
-  return (
-    <div className="App">
-      <Form />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Form />
+        </div>
+    );
 }
 
 export default App;
