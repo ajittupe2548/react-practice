c86a865b34147ee0da6f55bdeebc3415d9f4bfe0
cw-ajit <ajit@carwale.com>
2024-08-08 11:53:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,50 @@ import React, { useState, useEffect, useRef } from 'react';
 
 /*
 Making setInterval Declarative with React Hooks: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
-
-
 */
+
+/* First Attempt: Attach setInterval and clear on each render.
+
+This code has a strange behavior.
+When we run clearInterval and setInterval, their timing shifts. If we re-render and re-apply effects too often, the interval never gets a chance to fire!
+*/
+
+function Counter1() {
+    let [count, setCount] = useState(0);
+
+    useEffect(() => {
+        let id = setInterval(() => {
+            setCount(count + 1/*  */);
+        }, 1000);
+        return () => clearInterval(id);
+    });
+
+    return <h1>{count}</h1>;
+}
+
+/* Second Attempt: Using empty dependecy array without callback in setState
+
+The problem is that useEffect captures the count from the first render. It is equal to 0. We never re-apply the effect so the closure in setInterval always references the count from the first render, and count + 1 is always 1. Oops!
+
+- One way to fix it is to replace setCount(count + 1) with the “updater” form like setCount(c => c + 1).
+- Second way we can form closure with useEffect callback instead of interval callback.
+*/
+
+function Counter2() {
+    let [count, setCount] = useState(0);
+
+    useEffect(() => {
+        // let closureOutSideInterval = count;  // 2nd fix
+        let id = setInterval(() => {
+            setCount(count + 1);
+            // setCount(++closureOutSideInterval);  // 2nd fix
+            // setCount(prev => prev + 1);  // 1st fix
+        }, 1000);
+        return () => clearInterval(id);
+    }, []);
+
+    return <h1>{count}</h1>;
+}
 
 function useInterval(callback, delay) {
     const savedCallback = useRef();
@@ -38,12 +79,13 @@ function Counter() {
     }
 
     return (
-        <>
+        <div>
             <h1>{count}</h1>
             <input value={delay} onChange={handleDelayChange} />
-        </>
+        </div>
     );
 }
+
 
 class CounterClass extends React.Component {
     state = {
@@ -90,8 +132,10 @@ function App() {
 
     return (
         <>
-            <Counter />
-            <CounterClass />
+            First Try: <Counter1 />
+            Second Try: <Counter2 />
+            useInterval: <Counter />
+            ClassExample: <CounterClass />
         </>
     );
 }
