89abbf4f7758efeb97da59df66c4dd8cc9797890
cw-ajit <ajit@carwale.com>
2024-08-22 09:45:18 +0530

    Added: Fix with destructuring

--- src/components/ProfilePageClass.js
+++ src/components/ProfilePageClass.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
+/*
+In below example, the first behavior is the correct one. If I follow a person and then navigate to another person’s profile, my component shouldn’t get confused about who I followed. This class implementation is clearly buggy.
+
+This class method reads from this.props.user. Props are immutable in React so they can never change. However, this is, and has always been, mutable.
+*/
+
 class ProfilePage extends React.Component {
-    showMessage = () => {
-        alert('Followed ' + this.props.user);
+    showMessage = (user) => {
+        alert('Followed ' + user);
     };
 
     handleClick = () => {
-        setTimeout(this.showMessage, 3000);
+        // setTimeout(() => this.showMessage(this.props.user), 3000);
+        /* One way to do it would be to read this.props early during the event, and then explicitly pass them through into the timeout completion handler */
+        const { user } = this.props;
+        setTimeout(() => this.showMessage(user), 3000);
     };
 
     render() {
