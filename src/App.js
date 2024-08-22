c827c4df3b6697092bf6dd83136e85f6977d24dc
cw-ajit <ajit@carwale.com>
2024-08-22 18:08:14 +0530

    Added: Short desc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
 
+/*
+Writing Resilient Components: https://overreacted.io/writing-resilient-components/
+
+1. Don’t stop the data flow
+a. Don’t Stop the Data Flow in Rendering
+*/
+
 function App() {
   return (
     <div className="App">
