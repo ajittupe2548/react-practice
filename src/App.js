9df53ad1a06bdc6d318b4687e8e225657b4d36f6
cw-ajit <ajit@carwale.com>
2024-10-22 15:48:36 +0530

    styled-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: #BF4F74;
+  border-radius: 3px;
+  border: none;
+  color: white;
+`
 
 function App() {
   return (
     <div className="App">
+      <Button>Click</Button>
     </div>
   );
 }
