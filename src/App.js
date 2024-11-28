391c2820a2f986a88740c8acdc91c1e6e44eeb0c
Ajit Tupe <ajittupe2548@gmail.com>
2024-11-28 23:07:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
+import React, { useState, useDeferredValue, Suspense, useMemo } from "react";
+
+function SearchResults({ query }) {
+  const deferredQuery = useDeferredValue(query);
+
+  const list = useMemo(() => {
+    const l = [];
+    for (let i = 0; i < 50000; i++) {
+      l.push(<div key={i}>{deferredQuery}</div>)
+    }
+    return l;
+  }, [deferredQuery]);
+
+  return list;
+}
 
 function App() {
+  const [query, setQuery] = useState("");
+
   return (
-    <div className="App">
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search..."
+      />
+      <SearchResults query={query} />
     </div>
   );
 }
