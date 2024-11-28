7defa4a0a2106fe12894f3c4bd027d51530a0625
Ajit Tupe <ajittupe2548@gmail.com>
2024-11-28 22:45:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import React, { useState, useTransition } from "react";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [list, setList] = useState([]);
+  const [isPending, startTransition] = useTransition();
+
+  const handleSearch = (e) => {
+    const value = e.target.value;
+    setQuery(value);
+
+    startTransition(() => {
+      let list = [];
+      for (let i = 0; i < 50000; i++) {
+        list.push(value);
+      }
+      setList(list);
+    });
+  };
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <input
+        type="text"
+        value={query}
+        onChange={handleSearch}
+        placeholder="Search items..."
+      />
+      {isPending && <p>Loading...</p>}
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
