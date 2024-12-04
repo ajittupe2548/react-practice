efd4fdb3c703e6758bfe2b58f8f0ca2bf61ff3bc
Ajit Tupe <ajittupe2548@gmail.com>
2024-12-04 21:43:04 +0530

    Added: useImperativeHandle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import './App.css';
+import { forwardRef, useRef, useImperativeHandle } from 'react';
+
+const randomColor = () => `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256})`;
+
+const CustomInput = forwardRef((props, ref) => {
+  const inputRef = useRef();
+  const colorRef = useRef();
+
+  useImperativeHandle(ref, () => ({
+    focus: () => {
+      inputRef.current.focus();
+    },
+    clear: () => {
+      inputRef.current.value = "";
+    },
+    changeColor: () => {
+      colorRef.current.style.backgroundColor = randomColor();
+    }
+  }), []);
+
+  return (
+    <>
+      <input ref={inputRef} {...props} />
+      <div ref={colorRef}>Color</div>
+    </>
+  );
+});
 
 function App() {
+  const ref = useRef();
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <CustomInput ref={ref} placeholder="Type something..." />
+      <div />
+      <button onClick={() => ref.current.focus()}>Focus Input</button>
+      <button onClick={() => ref.current.clear()}>Clear Input</button>
+      <button onClick={() => ref.current.changeColor()}>Change bg color</button>
+    </>
   );
 }
 
