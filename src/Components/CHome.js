5954422399d60e60a085dbbb257b6ba29135523a
cw-ajit <ajit@carwale.com>
2024-12-09 17:45:59 +0530

    Added: Portal details

--- src/Components/CHome.js
+++ src/Components/CHome.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import PortalDemo from './PortalDemo';
 
-// Event Bubbling Through Portals - Even though a portal can be anywhere in the DOM tree, it behaves like a normal React child in every other way. Features like context work exactly the same regardless of whether the child is a portal, as the portal still exists in the React tree regardless of position in the DOM tree. This includes event bubbling. An event fired from inside a portal will propagate to ancestors in the containing React tree, even if those elements are not ancestors in the DOM tree.
-
 function Child() {
     return <button>Click</button>;
 }
--- src/Components/PortalDemo.js
+++ src/Components/PortalDemo.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+/*
+createPortal lets us render some children into a different part of the DOM.
+
+- A portal only changes the physical placement of the DOM node. In every other way, the JSX we render into a portal acts as a child node of the React component that renders it. For example, the child can access the context provided by the parent tree, and events bubble up from children to parents according to the React tree.
+
+`createPortal(children, domNode, key?)`
+- Parameters
+    - children: Anything that can be rendered with React, such as a piece of JSX (e.g. <div /> or <SomeComponent />), a Fragment (<>...</>), a string or a number, or an array of these.
+    - domNode: Some DOM node, such as those returned by document.getElementById(). The node must already exist. Passing a different DOM node during an update will cause the portal content to be recreated.
+    - optional key: A unique string or number to be used as the portal’s key.
+*/
+
 // function PortalDemo() {
 //     return ReactDOM.createPortal(
 //         <h3>This text is from portal demo component.</h3>,
