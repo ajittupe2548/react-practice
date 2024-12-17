c7405d34b070e0e4597bc741ad2944878001edcd
Ajit Tupe <ajittupe2548@gmail.com>
2024-12-17 13:12:54 +0530

    ProtectedRoutes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet, Navigate } from 'react-router-dom';
 
 const Header = () => (
   <div className='header'>
@@ -9,6 +9,8 @@ const Header = () => (
       <a href='/about' className='link'>About</a>
       <a href='/teams' className='link'>Teams</a>
       <a href='/login' className='link'>Login</a>
+      <a href='/profile' className='link'>Profile</a>
+      <a href='/cart' className='link'>Cart</a>
     </div>
   </div>
 )
@@ -17,6 +19,13 @@ const Home = () => <div>Home</div>
 const About = () => <div>About</div>
 const Teams = () => <div>Teams</div>
 const Login = () => <div>Login</div>
+const Profile = () => <div>Profile</div>
+const Cart = () => <div>Cart</div>
+
+const ProtectedRoutes = () => {
+  const isAuthenticated = true;
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
 
 function App() {
   return (
@@ -28,6 +37,10 @@ function App() {
           <Route path='/about' Component={About} />
           <Route path='/teams' Component={Teams} />
           <Route path='/login' Component={Login} />
+          <Route element={<ProtectedRoutes>asdf</ProtectedRoutes>}>
+            <Route path='/profile' Component={Profile} />
+            <Route path='/cart' Component={Cart} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
