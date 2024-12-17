eb87c7327f5dae30aa46bf7c040814af2d59232d
Ajit Tupe <ajittupe2548@gmail.com>
2024-12-17 14:13:16 +0530

    Added: More examples

--- src/App.js
+++ src/App.js
@@ -1,46 +1,77 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Outlet, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+  useParams,
+  NavLink,
+  Link,
+} from 'react-router-dom';
 
 const Header = () => (
   <div className='header'>
     <p>Header</p>
     <div className='links'>
-      <a href='/' className='link'>Home</a>
-      <a href='/about' className='link'>About</a>
-      <a href='/teams' className='link'>Teams</a>
-      <a href='/login' className='link'>Login</a>
-      <a href='/profile' className='link'>Profile</a>
-      <a href='/cart' className='link'>Cart</a>
+      <NavLink to="/" className='link'>
+        Home
+      </NavLink>
+      {/* Used link intentionally to check active class */}
+      <Link to='/about' className='link'>
+        About
+      </Link>
+      <NavLink to='/teams/frontend' className='link'>
+        Teams
+      </NavLink>
+      <NavLink to='/login' className='link'>
+        Login
+      </NavLink>
+      <NavLink to='/profile' className='link'>
+        Profile
+      </NavLink>
+      <NavLink to='/cart' className='link'>
+        Cart
+      </NavLink>
     </div>
   </div>
-)
+);
 
-const Home = () => <div>Home</div>
-const About = () => <div>About</div>
-const Teams = () => <div>Teams</div>
-const Login = () => <div>Login</div>
-const Profile = () => <div>Profile</div>
-const Cart = () => <div>Cart</div>
+const Home = () => <div>Home</div>;
+const About = () => <div>About</div>;
+const Teams = () => {
+  const { id } = useParams();
+  return <div>Teams - {id}</div>;
+};
+const Login = () => <div>Login</div>;
+const Profile = () => <div>Profile</div>;
+const Cart = () => <div>Cart</div>;
 
 const ProtectedRoutes = () => {
-  const isAuthenticated = true;
-  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
-}
+  const isAuthenticated = false;
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />;
+};
 
 function App() {
   return (
     <>
-      <Header />
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' Component={Home} />
           <Route path='/about' Component={About} />
-          <Route path='/teams' Component={Teams} />
+          {/* Dynamic path */}
+          <Route path='/teams/:id' Component={Teams} />
           <Route path='/login' Component={Login} />
-          <Route element={<ProtectedRoutes>asdf</ProtectedRoutes>}>
+          {/* Protected Route */}
+          <Route element={<ProtectedRoutes />}>
             <Route path='/profile' Component={Profile} />
             <Route path='/cart' Component={Cart} />
           </Route>
+          {/* Redirect example */}
+          <Route path='/old-route' element={<Navigate to='/about' />} />
+          {/* Fallback 404 Page */}
+          <Route path='*' element={<h1>404 - Page Not Found</h1>} />
         </Routes>
       </BrowserRouter>
     </>
