ae3de382dbec9394c0c8de5c518a563b098f86da
Ajit Tupe <ajittupe2548@gmail.com>
2024-12-17 12:20:03 +0530

    Added: React router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+
+const Header = () => (
+  <div className='header'>
+    <p>Header</p>
+    <div className='links'>
+      <a href='/' className='link'>Home</a>
+      <a href='/about' className='link'>About</a>
+      <a href='/teams' className='link'>Teams</a>
+      <a href='/login' className='link'>Login</a>
+    </div>
+  </div>
+)
+
+const Home = () => <div>Home</div>
+const About = () => <div>About</div>
+const Teams = () => <div>Teams</div>
+const Login = () => <div>Login</div>
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={Home} />
+          <Route path='/about' Component={About} />
+          <Route path='/teams' Component={Teams} />
+          <Route path='/login' Component={Login} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
