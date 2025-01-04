030c43602905c0508e710bb5e3244c2a63f113b3
Ajit Tupe <ajittupe2548@gmail.com>
2025-01-04 20:21:08 +0530

    Added: Redux components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import CakeContainer from './components/CakeContainer';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <UserContainer />
+        <ItemContainer />
+        <ItemContainer cake />
+        <CakeContainer />
+        <HooksCakeContainer />
+        <IceCreamContainer />
+        <NewCakeContainer />
+      </div>
+    </Provider>
   );
 }
 
